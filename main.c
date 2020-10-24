b1db11cfdf47787a3ce846b19827228719e5dbfe
gugrug <gustavorugg@gmail.com>
2020-10-24 13:11:56 -0300

    Starting more tests to understand flags behavior

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gumartin <gumartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 22:36:01 by gumartin          #+#    #+#             */
-/*   Updated: 2020/10/24 11:27:44 by gumartin         ###   ########.fr       */
+/*   Updated: 2020/10/24 13:10:17 by gumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	main(void)
 {
-	int a = 0;
-	ft_printf("Isso e um teste basico \n%d, %i, %u, %c, %s, %x, %X, %p\n"
-			, -546, 3834567899, 3834567899,'h', "abacate", 163, 32456, &a);
-	printf("\nnumero |%p|\n", &a);
+	int a = 5552; int hex = 161680; char c = 'W';
+
+	
+	//ft_printf("Isso e um teste basico \n%d, %i, %u, %c, %s, %x, %X, %p\n", -546, 3834567899, 3834567899,'h', "abacate", 163, 32456, &a);
+	ft_printf("\nft_printf |%d|    -    ", a);printf("|%d| Correto\n", a);
 	return (0);
 }
