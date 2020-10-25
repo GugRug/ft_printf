46db48b8503a3e343f656e265deeaa000a480e50
gugrug <gustavorugg@gmail.com>
2020-10-25 00:10:04 -0300

    finish basic test to understand flags behavior with specifiers and precision/wight

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gumartin <gumartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 22:36:01 by gumartin          #+#    #+#             */
-/*   Updated: 2020/10/24 13:10:17 by gumartin         ###   ########.fr       */
+/*   Updated: 2020/10/25 00:08:34 by gumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	main(void)
 {
-	int a = 5; unsigned int un = 5; char c = 'W'; int he = 17080; 
+	int a = 5, b = 70; unsigned int un = 5; char c = 'W'; int he = 17080; 
 	char *str = "test71";
 	int *addr = &a;
 	// printf("Isso e o pf %.5- real \n%d, %i, %u, %c, %s, %x, %X, %p\n", -234, 3834567899, 3834567899,'h', "abacate", 163, 32456, &a);
 	// ft_printf("Isso e um %.5- basico \n%d, %i, %u, %c, %s, %x, %X, %p\n", -234, 3834567899, 3834567899,'h', "abacate", 163, 32456, &a);
 	ft_printf("INTEGERS\n");
-	ft_printf("ft_printf |%-*d|	-	", 3, a);printf("|%-*d| Correto\n", 3, a);
+	ft_printf("ft_printf |%-*d|	-	", 3, a);printf("|%-*d| Correto\n", 3, a, b);
 	ft_printf("ft_printf |%05d|	-	", a);printf("|%05d| Correto\n", a);
 	ft_printf("ft_printf |%-05d|	-	", a);printf("|%-05d| Correto\n", a);
-	ft_printf("ft_printf |%.9d|	-	", a);printf("|%-.9d| Correto\n", a);
+	ft_printf("ft_printf |%.9d|	-	", a);printf("|%.9d| Correto\n", a);
 	ft_printf("ft_printf |%-.9d|	-	", a);printf("|%-.9d| Correto\n", a);
 	ft_printf("ft_printf |%5.2d|	-	", a);printf("|%5.2d| Correto\n", a);
 	ft_printf("ft_printf |%010.2d|	-	", a);printf("|%010.2d| Correto\n", a);
@@ -66,13 +66,13 @@ int	main(void)
 	ft_printf("ft_printf |%010.8s|	-	", str);printf("|%010.8s| Correto\n", str);
 
 	ft_printf("Adress -> %p\n", addr);
-	ft_printf("ft_printf |%*p|	-	", 8, addr);printf("|%*p| Correto\n", 20, addr);
-	ft_printf("ft_printf |%050p|	-	", addr);printf("|%050p| Correto\n", addr);
+	ft_printf("ft_printf |%-*p|	-	", 8, addr);printf("|%-*p| Correto\n", 20, addr);
+	ft_printf("ft_printf |%050.20p|	-	", addr);printf("|%050.20p| Correto\n", addr);
 	ft_printf("ft_printf |%-050p|	-	", addr);printf("|%-050p| Correto\n", addr);
 	ft_printf("ft_printf |%.90p|	-	", addr);printf("|%.90p| Correto\n", addr);
-	ft_printf("ft_printf |%-.9p|	-	", addr);printf("|%.-9p| Correto\n", addr);
+	ft_printf("ft_printf |%-.9p|	-	", addr);printf("|%-.9p| Correto\n", addr);
 	ft_printf("ft_printf |%5.4p|	-	", addr);printf("|%5.4p| Correto\n", addr);
-	ft_printf("ft_printf |%050.8p|	-	", addr);printf("|%050.8p| Correto\n", addr);
+	ft_printf("ft_printf |%50.300p|	-	", addr);printf("|%50.300p| Correto\n", addr);
 
 	return (0);
 }
